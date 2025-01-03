7b1a4d5e65ecd712508788e96d49391d8489d6da
harshitha9988 <harshitha.uppala@outlook.com>
2025-01-02 21:52:10 -0500

    storing birthdays program

---
+++ birthdays.py
@@ -0,0 +1,8 @@
+mom="August 8th"
+dad="May 12th"
+me="May 6th"
+sis="July 29th"
+print ("my birthday is on", me)
+print ("my Mom's birthday is on", mom)
+print ("my Dad's birthday is on", dad)
+print ("my sister's birthday is on", sis)
